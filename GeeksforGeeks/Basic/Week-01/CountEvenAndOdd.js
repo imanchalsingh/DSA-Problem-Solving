64cb7b364fe65f0165d7242dea37bf61efbd9060
anchalDev159357 <singhanchal159357@gmail.com>
2025-01-07 11:06:51 +0530

    added changes

--- GeeksforGeeks/Basic/Week-01/CountEvenAndOdd.js
+++ GeeksforGeeks/Basic/Week-01/CountEvenAndOdd.js
@@ -1,1 +1,20 @@
 // Count Even and Odd in array. time complexity O(n)
+class Solution {
+  countOddEven(arr) {
+    let even = 0;
+    let odd = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        even++;
+      } else {
+        odd++;
+      }
+    }
+    return [even, odd];
+  }
+}
+
+// Example usage:
+const solution = new Solution();
+console.log(solution.countOddEven([1, 2, 3, 4, 5, 6]));
+console.log(solution.countOddEven([1, 3, 5]));
