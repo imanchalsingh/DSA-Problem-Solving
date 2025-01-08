5b9e42fd75dd142bbf6e6d8a156b00a349afd6b6
anchalDev159357 <singhanchal159357@gmail.com>
2025-01-08 16:49:34 +0530

    added changes

---
+++ LeetCode/Easy/Week-02/SumArrayPuzzle.js
@@ -0,0 +1,12 @@
+class Solution {
+  sumArray(arr) {
+    const totalSum = arr.reduce((sum, num) => sum + num, 0);
+
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = totalSum - arr[i];
+    }
+
+    return arr;
+  }
+}
+
