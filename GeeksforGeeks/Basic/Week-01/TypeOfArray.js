d17a86e353f05f8bb6147d34cf147bf40062baa1
anchalDev159357 <singhanchal159357@gmail.com>
2025-01-08 10:36:49 +0530

    added changes

--- GeeksforGeeks/Basic/Week-01/TypeOfArray.js
+++ GeeksforGeeks/Basic/Week-01/TypeOfArray.js
@@ -1,27 +1,34 @@
 class Solution {
-    maxNtype(arr) {
-        let n = arr.length;
-        let dips = 0;
-        let peaks = 0;
-
-        for (let i = 0; i < n; i++) {
-            let next = arr[(i + 1) % n];
-            if (arr[i] > next) dips++;
-            if (arr[i] < next) peaks++;
-        }
-
-        if (dips === 0 && peaks === n - 1) return 1;
-        if (peaks === 0 && dips === n - 1) return 2;
-        if (dips === 1 && peaks > 0) return 4;
-        if (peaks === 1 && dips > 0) return 3;
-
-        return -1;
+  maxNtype(arr) {
+    let n = arr.length;
+    let max = arr[0];
+    let min = arr[0];
+    let maxIndex = 0;
+    let minIndex = 0;
+    for (let i = 0; i < n; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+        maxIndex = i;
+      }
+      if (arr[i] < min) {
+        min = arr[i];
+        minIndex = i;
+      }
     }
+    if (maxIndex === n - 1 && minIndex === 0) {
+      return 1;
+    }
+    if (maxIndex === 0 && minIndex === n - 1) {
+      return 2;
+    }
+    if (maxIndex !== n - 1 && minIndex !== 0) {
+      return 3;
+    }
+    return 3;
+  }
 }
 
-// Example Usage:
+// Example usage:
 const solution = new Solution();
-console.log(solution.maxNtype([2, 1, 5, 4, 3])); 
-console.log(solution.maxNtype([3, 4, 5, 1, 2])); 
 console.log(solution.maxNtype([1, 2, 3, 4, 5]));
 console.log(solution.maxNtype([5, 4, 3, 2, 1]));
