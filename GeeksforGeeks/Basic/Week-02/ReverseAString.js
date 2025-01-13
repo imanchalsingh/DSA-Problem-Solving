c36a27b3d66414ba19ec0856a2e6e8558298b119
anchalDev159357 <singhanchal159357@gmail.com>
2025-01-13 16:34:36 +0530

    added solutions

---
+++ GeeksforGeeks/Basic/Week-02/ReverseAString.js
@@ -0,0 +1,7 @@
+function reverseString(s) {
+  return s.split("").reverse().join("");
+}
+
+// Example usage:
+console.log(reverseString("string"));
+console.log(reverseString("solutions"));
---
+++ GeeksforGeeks/Basic/Week-02/ReverseAStringII.js
@@ -0,0 +1,7 @@
+function reverseString(s) {
+  return s.split("").reverse().join("");
+}
+
+// Example usage:
+console.log(reverseString("hello"));
+console.log(reverseString("world"));
