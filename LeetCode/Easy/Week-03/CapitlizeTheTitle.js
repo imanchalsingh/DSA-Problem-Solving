5165c994726de149667100fa82e21d92b852f721
anchalDev159357 <singhanchal159357@gmail.com>
2025-01-15 17:52:33 +0530

    added changes

---
+++ LeetCode/Easy/Week-03/CapitlizeTheTitle.js
@@ -0,0 +1,5 @@
+var capitalizeTitle = function (title) {
+  return title.toLowerCase();
+};
+
+console.log(capitalizeTitle("CaPItaLiZe tHe TiTle"));
---
+++ LeetCode/Easy/Week-03/ReverseString.js
@@ -0,0 +1,5 @@
+var reverseString = function (s) {
+  return s.reverse();
+};
+
+console.log(reverseString("hello"));
