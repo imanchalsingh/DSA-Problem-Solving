3aec84824ed650720733c4ac52a2f2486cdb3540
anchalDev159357 <singhanchal159357@gmail.com>
2025-01-25 17:29:15 +0530

    Added solutions

---
+++ LeetCode/Easy/Week-04/AddBinary.js
@@ -0,0 +1,20 @@
+var addBinary = function (a, b) {
+  let result = "";
+  let carry = 0;
+  let i = a.length - 1,
+    j = b.length - 1;
+
+  while (i >= 0 || j >= 0 || carry > 0) {
+    const bitA = i >= 0 ? parseInt(a[i]) : 0;
+    const bitB = j >= 0 ? parseInt(b[j]) : 0;
+
+    const sum = bitA + bitB + carry;
+    result = (sum % 2) + result;
+    carry = Math.floor(sum / 2);
+
+    i--;
+    j--;
+  }
+
+  return result;
+};
