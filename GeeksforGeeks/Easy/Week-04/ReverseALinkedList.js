701a69c10e41f734a578cb2dfecb9b3f5ce518cc
anchalDev159357 <singhanchal159357@gmail.com>
2025-01-31 12:23:43 +0530

    added solutions

---
+++ GeeksforGeeks/Easy/Week-04/ReverseALinkedList.js
@@ -0,0 +1,15 @@
+class Solution {
+  reverseList(head) {
+    let prev = null;
+    let current = head;
+
+    while (current !== null) {
+      let nextNode = current.next;
+      current.next = prev;
+      prev = current;
+      current = nextNode;
+    }
+
+    return prev;
+  }
+}
