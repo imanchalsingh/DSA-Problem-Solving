b765f94f98ceab937c653d761c33b554ec7d5c32
anchalDev159357 <singhanchal159357@gmail.com>
2025-04-28 15:05:56 +0530

    commit changes

--- CPPgfg/Dsa/RectanglePattern.cpp
+++ CPPgfg/Dsa/RectanglePattern.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int main()
 {
     int rn, cn;
+    string ps;
     cout << "Enter Row number: ";
     cin >> rn;
     cout << "Enter Column number: ";
     cin >> cn;
+    cout << "What you wanna print(String): ";
+    cin >> ps;
     for (int i = 1; i <= rn; i++)
     {
         for (int j = 1; j <= cn; j++)
         {
-            cout << "* ";
+            cout << ps << " ";
         }
         cout << endl;
     }
