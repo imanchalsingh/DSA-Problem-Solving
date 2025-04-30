ea38367ef9fe8af5f7ec91adf4def684d0b71691
anchalDev159357 <singhanchal159357@gmail.com>
2025-04-30 17:53:26 +0530

    added solutions

---
+++ CPPgfg/Dsa/PowerOfANumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    cout << pow(a, b) << endl;
+    return 0;
+}
