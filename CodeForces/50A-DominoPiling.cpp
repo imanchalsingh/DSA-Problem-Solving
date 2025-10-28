11f0d9ca69f7c0e89ad07750edb436098b6b698b
imanchalsingh <singhanchal159357@gmail.com>
2025-10-28 21:54:43 +0530

    added solutions

---
+++ CodeForces/50A-DominoPiling.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int maxDominoes = (n * m) / 2;
+    cout << maxDominoes << endl;
+
+    return 0;
+}
